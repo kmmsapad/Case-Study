024eaad6bab803d604800a91aebb0f89bc9c586d
kmmsapad <81031338+kmmsapad@users.noreply.github.com>
2021-03-27 23:09:21 +0800

    Add files via upload

---
+++ forloop1.java
@@ -0,0 +1,22 @@
+public class forloop1
+{
+	public static void main(String[] args)
+	{
+
+		for (int c=10;c>=10;c--)
+		{
+			for (int d=1;d<=Math.abs(c);d++)
+			{ 
+			System.out.print("");
+			}
+		
+		for(int k=10;c>=Math.abs;c--)
+		{
+			
+			System.out.print("*");
+		}
+			System.out.println();
+		
+		}
+	}
+}
